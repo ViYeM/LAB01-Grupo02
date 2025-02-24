d281b485c2844cc6b475725f285ff98e96122146
JoaquimT6 <104275009+JoaquimT6@users.noreply.github.com>
2025-02-23 22:08:37 -0300

    #2

--- github-analysis/query_github.py
+++ github-analysis/query_github.py
@@ -1,46 +1,29 @@
 import requests
-import pandas as pd
 
-GITHUB_TOKEN = "Github Token"
+GITHUB_TOKEN = "github token"
+
 GITHUB_API_URL = "https://api.github.com/graphql"
+
 headers = {
     "Authorization": f"Bearer {GITHUB_TOKEN}",
     "Content-Type": "application/json"
 }
 
-def fetch_repositories():
-    all_repos = []
-    cursor = None
+query = """
+{
+  search(query: "stars:>10000", type: REPOSITORY, first: 100) {
+    nodes {
+      ... on Repository {
+        nameWithOwner
+        createdAt
+        primaryLanguage { name }
+        updatedAt
+      }
+    }
+  }
+}
+"""
 
-    for _ in range(10): 
-        query = f"""
-        {{
-          search(query: "stars:>10000", type: REPOSITORY, first: 100, after: {cursor if cursor else "null"}) {{
-            nodes {{
-              ... on Repository {{
-                nameWithOwner
-                createdAt
-                primaryLanguage {{ name }}
-                updatedAt
-              }}
-            }}
-            pageInfo {{ endCursor hasNextPage }}
-          }}
-        }}
-        """
-        response = requests.post(GITHUB_API_URL, json={"query": query}, headers=headers).json()
-        repos = response["data"]["search"]["nodes"]
-        all_repos.extend(repos)
+response = requests.post(GITHUB_API_URL, json={"query": query}, headers=headers)
 
-        if not response["data"]["search"]["pageInfo"]["hasNextPage"]:
-            break
-        cursor = f'"{response["data"]["search"]["pageInfo"]["endCursor"]}"'
-
-    return all_repos
-
-repos = fetch_repositories()
-
-df = pd.DataFrame(repos)
-
-df.to_csv("github_repositories.csv", index=False)
-print("Dados salvos em github_repositories.csv")
+print(response.json())
